a47bb69d063134bc28609663f79844cc4ce139bf
Aleksei Sitnikov <alexsitnikov2003@gmail.com>
2023-02-05 18:15:22 +0300

    feat: realize create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,14 @@
+import fs from "fs";
+
 const create = async () => {
-    // Write your code here 
+  const file = "./src/fs/files/fresh.txt";
+  if (fs.existsSync(file)) {
+    throw Error("FS operation failed");
+  }
+  fs.writeFile("./src/fs/files/fresh.txt", "I am fresh and young", (err) => {
+    if (err) throw err;
+    console.log("Saved!");
+  });
 };
 
 await create();
